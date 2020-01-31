3213a9ba134d8e525a52b7a3ae1f8d0ec5d9deeb
Katy Solovewicz <katy@solovewicz.com>
2020-01-30 21:48:26 -0800

    add functionality to New Game button

--- js/script.js
+++ js/script.js
@@ -1,120 +1,135 @@
 // Business-Logic
 
+// Constructor for creating a new game
 function Game() {
-    this.activePlayer = "X";
-    this.boardSquares = [];
+  this.activePlayer = "X";
+  this.boardSquares = [];
 }
 
+// Constructor for creating new squares
 function Square(div, value) {
-    this.div = div;
-    this.value = value;
+  this.div = div;
+  this.value = value;
 }
 
-function Values() {
-    this.sq0 = sq0;
-    this.sq1 = sq1;
-    this.sq2 = sq2;
-    this.sq3 = sq3;
-    this.sq4 = sq4;
-    this.sq5 = sq5;
-    this.sq6 = sq6;
-    this.sq7 = sq7;
-    this.sq8 = sq8;
-}
-
-// Assign Values to Squares
-Values.prototype.addValue = function(values, div){
-    values.div = div;
-    console.log("values div: " + values.sq0);
-}
-
-// Assign Square to Game
-Game.prototype.addSquare = function (game, div) {
+// Fill square variables and push them into game
+Game.prototype.addSquare = function(game, div) {
     var value = game.activePlayer;
     var square = new Square(div, value);
     this.boardSquares.push(square);
 }
 
-// Apply Mark
-// print X in selected div ID
-// assign X to div ID in JSON object
-Game.prototype.applyMark = function (game) {
+// Apply user's mark on html page
+Game.prototype.applyMark = function(game) {
     var element = game.boardSquares.length - 1;
     var div = game.boardSquares[element].div;
     var value = game.boardSquares[element].value;
     $("#" + div).html(value);
-
 }
 
-// checkForWinner
-// check for like values existing in winning combinations
-// if all boxes are filled with no winning combinations, game is over
-Game.prototype.checkForWinner = function (game) {};
+// // checkForWinner
+// Game.prototype.checkForWinner = function(game) {
+
+//   // Establish winning array combinations
+//   winningArrays = [["sq0", "sq1", "sq2"], ["sq0", "sq3", "sq6"], ["sq0", "sq4", "sq8"], ["sq1", "sq4", "sq7"], ["sq2", "sq5", "sq8"], ["sq2", "sq4", "sq7"], ["sq3", "sq4", "sq5"], ["sq6", "sq7", "sq8"]];
+  
+//   // Declare variables passed in from game
+//   var activePlayer = game.activePlayer;
+//   var element = game.boardSquares.length - 1;
+//   var div = game.boardSquares[element].div;
+//   var value = game.boardSquares[element].value;
+//   var winningArrayValue = [];
+//   var potentialWinningCombos = [];
 
 
-//     winningArrays = [["sq0", "sq1", "sq2"], ["sq0", "sq3", "sq6"], ["sq0", "sq4", "sq8"], ["sq1", "sq4", "sq7"], ["sq2", "sq5", "sq8"], ["sq2", "sq4", "sq7"], ["sq3", "sq4", "sq5"], ["sq6", "sq7", "sq8"]];
-//     var element = game.boardSquares.length - 1;
-//     var div = game.boardSquares[element].div;
-//     var value = game.boardSquares[element].value;
-//     console.log("game: " + game)
-//     winningArrays.forEach(function (winningArray) {
-//         if (winningArray.includes(div)) {
-//             console.log("winning array: " + winningArray)
-//             if (winningArray[0] == winningArray[1] && winningArray[0] == winningArray[2]) {
-//                 var winner = game.activePlayer;
-//                 alert(winner);
-//             }
-//         };
-//     });
-// }
+
+  
+//   // Cycle through each potential winning array
+//   winningArrays.forEach(function(winningArray) {
+//     // Narrow down which winning arrays to check
+//     if (winningArray.includes(div)) {
+//       potentialWinningCombos.push(winningArray);
+//     }
+
+//     // For each array in PWC
+//     potentialWinningCombos.foreach(potentialWC) {
+//       // Look in every entered move in game
+//       for (i = 0; i < game.boardSquares.length; i++) {
+//         // Check for squares previously marked by Active Player
+//         if (game.boardSquares[i].value === activePlayer) {
+          
+//         }
+//         for(x = 0; x < winningArray.length; x++) {
+
+//         }
+//       }
+//     }
+
+
+
+      // Cycle through all the gameboard squares to find the current square clicked
+      for (i = 0; i < game.boardSquares.length; i++) {
+        for(x = 0; x < winningArray.length; x++) {
+          if (winningArray[x] === div) {
+            // Get the value from the current square clicked
+            console.log("x: " + x);
+            winningArrayValue[x] = game.boardSquares[i].value;
+            console.log("winningArrayValue: " + winningArrayValue);
+            console.log("winningArray[x]: " + winningArray[x]);
+            if (winningArrayValue[0] === value && winningArrayValue[1] === value && winningArrayValue[2] === value) {
+              var winner = game.activePlayer;
+              alert(winner);
+            }
+          }
+        }  
+      }
+    };
+  });
+}
 
 // Game moves turn to next Player
-Game.prototype.nextTurn = function (game) {
-    if (game.activePlayer === "X") {
-        game.activePlayer = "O";
-    } else {
-        game.activePlayer = "X";
-    }
+Game.prototype.nextTurn = function(game) {
+  if (game.activePlayer === "X") {
+      game.activePlayer = "O";
+  } else {
+      game.activePlayer = "X";
+  }
 }
 
-// function newGame(){
-//     var game = new Game;
-//         $("#running-total").html("0");
-//         $("#player1-score").html("0");
-//         $("#player2-score").html("0");
-//         $("#roll-result").html("0");
-//         $("#player1-panel").css("background-color", "lightblue");
-// }
+// Reset the game variables and the DOM
+function newGame(){
+  var game = new Game;
+  $(".square").html("");
+  alert("X starts first!");
+}
 
 //User-Logic
 
 $(document).ready(function() {
-    //var square = new Square();
-    var game = new Game();
-    var values = new Values();
-    // assign Square to Game
-    $(".square").hover(function() {
-        var div = event.target.id;
-        $("#" + div).css("background-color", "lightblue");
-    } , function() {
-        var div = event.target.id;
-        $("#" + div).css("background-color", "whitesmoke");
-    })
+  
+  // Start new game
+  var game = new Game();
 
-    // $("#player1-panel").css("background-color", "lightblue");
-    $(".game-board").click(function() {
-        var div = event.target.id;
-        values.addValue(values, div);
-        game.addSquare(game, div);
-        game.applyMark(game);
-        game.checkForWinner(game, div);
-        game.nextTurn(game);
+  // Game board highlights when user hovers over a div
+  $(".square").hover(function() {
+    var div = event.target.id;
+    $("#" + div).css("background-color", "lightblue");
+  }, function() {
+    var div = event.target.id;
+    $("#" + div).css("background-color", "whitesmoke");
+  });
 
-        console.log(game);
+  // When user clicks a game tile, the following commands will execute
+  $(".game-board").click(function() {
+    var div = event.target.id;
+    game.addSquare(game, div);
+    game.applyMark(game);
+    game.checkForWinner(game, div);
+    game.nextTurn(game);
+  });
 
-        // $("#new-game").click(function() {
-        // alert("X starts first!");
-        //      newGame();
-        // });
-    });
+  // Allows user to start a new game when pressing a new game button
+  $("#new-game").click(function() {
+    newGame();
+  });
 });
